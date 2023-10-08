bf38981abe12eee830d42083363bd46cd4400548
The YOLO Studio <studiotheyolo@gmail.com>
2023-10-08 20:17:52 +0600

    fix: solved

--- src/components/provider/TanstackProvider.tsx
+++ src/components/provider/TanstackProvider.tsx
@@ -1,21 +1,19 @@
-// 'use client';;
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       suspense: false,
-//     },
-//   },
-// });
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: false,
+    },
+  },
+});
 
-// const TanStackQueryProvider = ({ children }:{children: React.ReactNode;}) => {
-//   return (
-//     <>
-//       <QueryClientProvider client={queryClient}>
-//             {children}
-//       </QueryClientProvider>
-//     </>
-//   );
-// };
+const TanStackQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </>
+  );
+};
 
-// export default TanStackQueryProvider;
+export default TanStackQueryProvider;
