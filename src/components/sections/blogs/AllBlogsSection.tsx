52326fef4abc57209671ccb9f36e1f8e35278fde
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2023-10-11 17:42:44 +0600

    style: worked on blog page

--- src/components/sections/blogs/AllBlogsSection.tsx
+++ src/components/sections/blogs/AllBlogsSection.tsx
@@ -2,14 +2,91 @@
 import React from 'react';
 import BlogCard from '../../core/cart/BlogCard';
 import { BlogType, useGetBlogData } from '@/hooks/querey/blog.tsq';
-
-const AllBlogsSection = () => {
-  const { data } = useGetBlogData();
+import LoadingCard from '@/components/core/cart/LoadingCard';
+import AddCard from '@/components/core/cart/AddCard';
+const AllBlogsSection = ({ addData }: { addData: any }) => {
+  const { data, isLoading } = useGetBlogData();
 
   return (
     <div>
       <div className="grid grid-cols-2 gap-5 mt-10 md:mt-12 md:grid-cols-2 xl:grid-cols-3 xl:mt-16 xl:gap-7">
-        {data && data?.results.map((i: BlogType) => <BlogCard key={i.id} blog={i} />)}
+        {isLoading
+          ? [...new Array(4)].map(() => (
+              <LoadingCard key={Math.random()} width={295} height={239} />
+            ))
+          : data?.results.slice(0, 6).map((i: BlogType) => <BlogCard key={i.id} blog={i} />)}
+      </div>
+      <div className="my-8 md:my-10 xl:my-[60px]">
+        {addData?.results?.[1]?.content?.blog_add5 ? (
+          <AddCard
+            image={addData?.results?.[1]?.content?.blog_add5?.add_banner}
+            width={945}
+            height={257}
+          />
+        ) : (
+          <LoadingCard width={945} height={257} />
+        )}
+      </div>
+
+      {/* mbl add show */}
+      <div className="grid grid-cols-2 px-2 gap-5 md:hidden">
+        <div className="mb-5 md:mb-8 xl:mb-14">
+          {addData?.results?.[1]?.content?.blog_add1 ? (
+            <AddCard
+              image={addData?.results?.[1]?.content?.blog_add1?.add_banner}
+              width={295}
+              height={488}
+            />
+          ) : (
+            <LoadingCard width={295} height={488} />
+          )}
+        </div>
+        <div className="mb-5 md:mb-8 xl:mb-14">
+          {addData?.results?.[1]?.content?.blog_add2 ? (
+            <AddCard
+              image={addData?.results?.[1]?.content?.blog_add2?.add_banner}
+              width={295}
+              height={668}
+            />
+          ) : (
+            <LoadingCard width={295} height={668} />
+          )}
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-5 mt-10 md:mt-12 md:grid-cols-2 xl:grid-cols-3 xl:mt-16 xl:gap-7">
+        {isLoading
+          ? [...new Array(4)].map(() => (
+              <LoadingCard key={Math.random()} width={295} height={239} />
+            ))
+          : data?.results
+              .slice(7, data.length)
+              .map((i: BlogType) => <BlogCard key={i.id} blog={i} />)}
+      </div>
+
+      {/* mbl bottomAdd section */}
+      <div className="grid grid-cols-2 gap-5 px-2 mt-7 md:hidden">
+        <div className="mb-5 md:mb-8 xl:mb-14">
+          {addData?.results?.[1]?.content?.blog_add3 ? (
+            <AddCard
+              image={addData?.results?.[1]?.content?.blog_add3?.add_banner}
+              width={295}
+              height={302}
+            />
+          ) : (
+            <LoadingCard width={295} height={302} />
+          )}
+        </div>
+        <div className="mb-5 md:mb-8 xl:mb-14">
+          {addData?.results?.[1]?.content?.blog_add4 ? (
+            <AddCard
+              image={addData?.results?.[1]?.content?.blog_add4?.add_banner}
+              width={295}
+              height={426}
+            />
+          ) : (
+            <LoadingCard width={295} height={426} />
+          )}
+        </div>
       </div>
     </div>
   );
