7fae1a0834742e89c9a853e953697db083c7ead0
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2023-12-18 15:52:20 +0600

    feat: added menu in mobile aside

--- src/components/core/navbar/MobileHamburgerMenu.tsx
+++ src/components/core/navbar/MobileHamburgerMenu.tsx
@@ -1,16 +1,68 @@
 'use client';
 import { Accordion, AccordionDetails, AccordionSummary, Drawer, Typography } from '@mui/material';
 import Image from 'next/legacy/image';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { RxCross2 } from 'react-icons/rx';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Link from 'next/link';
 import { useGetCategoryData } from '@/hooks/querey/category.tsq';
 
+const Menu = ({ menu, open, setOpen }: { menu: any; open: any; setOpen: any }) => {
+  const { data, isLoading } = useGetCategoryData(menu);
+  return (
+    <Accordion
+      sx={{
+        width: '280px',
+        boxShadow: 'none',
+        borderRadius: 'none',
+        margin: '-8px 0',
+      }}
+    >
+      <AccordionSummary
+        sx={{ padding: '0px' }}
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography
+          sx={{
+            fontSize: '18px',
+            fontWeight: '600',
+            margin: '0px',
+            padding: '0px',
+            // color:`${active?'#EAA41D':''}`
+          }}
+        >
+          {menu}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails sx={{ padding: '0px' }}>
+        <ul className="space-y-3 font-medium ml-10">
+          {isLoading ? (
+            <p className="h-4 w-full rounded-md"></p>
+          ) : (
+            data?.map((i: any, idx: number) => (
+              <li
+                className="list-disc"
+                onClick={() => setOpen(!open)}
+                // className={`list-disc ${router.pathname == i.url?'active_nav':''}`}
+                key={idx}
+              >
+                <Link className="" key={Math.random()} href={`/blog/?menu=${menu}&c_id=${i.id}`}>
+                  {i.title}
+                </Link>
+              </li>
+            ))
+          )}
+        </ul>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
 const MobileHamburgerMenu = () => {
   const [open, setOpen] = useState(false);
-  const { data } = useGetCategoryData();
   return (
     <div>
       <GiHamburgerMenu onClick={() => setOpen(true)} size={30} className="md:hidden" />
@@ -37,66 +89,15 @@ const MobileHamburgerMenu = () => {
             <ul className="text-xl font-semibold">
               <li>হোম</li>
             </ul>
-            {data?.results?.map((i: any) => {
-              return (
-                <div key={Math.random()}>
-                  {i.sub_category.length > 0 ? (
-                    <Accordion
-                      sx={{
-                        width: '288px',
-                        boxShadow: 'none',
-                        borderRadius: 'none',
-                        margin: '-8px 0',
-                      }}
-                    >
-                      <AccordionSummary
-                        sx={{ padding: '0px' }}
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                      >
-                        <Typography
-                          sx={{
-                            fontSize: '20px',
-                            fontWeight: '600',
-                            margin: '0px',
-                            padding: '0px',
-                            // color:`${active?'#EAA41D':''}`
-                          }}
-                        >
-                          {i.title}
-                        </Typography>
-                      </AccordionSummary>
-                      <AccordionDetails sx={{ padding: '0px' }}>
-                        <ul className="space-y-3 font-medium ml-10">
-                          {i?.sub_category?.map((j: { title: string }, idx: number) => (
-                            <li
-                              className="list-disc"
-                              onClick={() => setOpen(!open)}
-                              // className={`list-disc ${router.pathname == i.url?'active_nav':''}`}
-                              key={idx}
-                            >
-                              <Link href={`/blog/${i.title}/${j.title}`}>{j.title}</Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </AccordionDetails>
-                    </Accordion>
-                  ) : (
-                    <li className="text-xl list-none font-semibold">
-                      <Link href={`/blog/${i.title}`}>{i.title}</Link>
-                    </li>
-                  )}
-
-                  {/* <p>{instance.title}</p>
-      <ul className="mt-2 text-sm font-medium ml-5">
-        {
-          instance.submenu.map((i,idx)=> <li key={idx}><Link href={i.url}>{i.title}</Link></li>)
-        }
-      </ul> */}
-                </div>
-              );
-            })}
+            {['স্থাপত্য', 'প্রকৌশল', 'ইন্টেরিয়র', 'পরিবেশ', 'মূল রচনা', 'অন্যান্য'].map(
+              (i: any) => {
+                return (
+                  <div key={Math.random()}>
+                    <Menu menu={i} open={open} setOpen={setOpen} />
+                  </div>
+                );
+              },
+            )}
           </div>
         </div>
       </Drawer>
@@ -105,3 +106,64 @@ const MobileHamburgerMenu = () => {
 };
 
 export default MobileHamburgerMenu;
+
+// {data?.results?.map((i: any) => {
+//   return (
+//     <div key={Math.random()}>
+//       {i.sub_category.length > 0 ? (
+//         <Accordion
+//           sx={{
+//             width: '288px',
+//             boxShadow: 'none',
+//             borderRadius: 'none',
+//             margin: '-8px 0',
+//           }}
+//         >
+//           <AccordionSummary
+//             sx={{ padding: '0px' }}
+//             expandIcon={<ExpandMoreIcon />}
+//             aria-controls="panel1a-content"
+//             id="panel1a-header"
+//           >
+//             <Typography
+//               sx={{
+//                 fontSize: '20px',
+//                 fontWeight: '600',
+//                 margin: '0px',
+//                 padding: '0px',
+//                 // color:`${active?'#EAA41D':''}`
+//               }}
+//             >
+//               {i.title}
+//             </Typography>
+//           </AccordionSummary>
+//           <AccordionDetails sx={{ padding: '0px' }}>
+//             <ul className="space-y-3 font-medium ml-10">
+//               {i?.sub_category?.map((j: { title: string }, idx: number) => (
+//                 <li
+//                   className="list-disc"
+//                   onClick={() => setOpen(!open)}
+//                   // className={`list-disc ${router.pathname == i.url?'active_nav':''}`}
+//                   key={idx}
+//                 >
+//                   <Link href={`/blog/${i.title}/${j.title}`}>{j.title}</Link>
+//                 </li>
+//               ))}
+//             </ul>
+//           </AccordionDetails>
+//         </Accordion>
+//       ) : (
+//         <li className="text-xl list-none font-semibold">
+//           <Link href={`/blog/${i.title}`}>{i.title}</Link>
+//         </li>
+//       )}
+
+//       {/* <p>{instance.title}</p>
+// <ul className="mt-2 text-sm font-medium ml-5">
+// {
+// instance.submenu.map((i,idx)=> <li key={idx}><Link href={i.url}>{i.title}</Link></li>)
+// }
+// </ul> */}
+//     </div>
+//   );
+// })}
